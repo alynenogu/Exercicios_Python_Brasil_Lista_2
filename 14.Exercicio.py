83a46bc894e5975371cba63729b82d88dd903c94
alyne nogueira <alynenogueiragois@gmail.com>
2024-02-02 16:16:15 -0300

    Adição da função status

--- 14.Exercicio.py
+++ 14.Exercicio.py
@@ -6,6 +6,8 @@
 #  Entre 6.0 e 7.5         C
 #  Entre 4.0 e 6.0         D
 #  Entre 4.0 e zero        E
+#O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a 
+#mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
 
 def digita_nota(mensagem):
     nota = float(input(mensagem))
@@ -29,12 +31,22 @@ def verifica_conceito (media):
     elif media >=4.0 and media<6.0:
         conceito = "D"
         return conceito
-    elif media >4.0 :
+    elif media <4.0 :
         conceito = "E"
         return conceito
+    
+def verifica_status(conceito):
+    if conceito == "A" or conceito =="B" or conceito == "C":
+        status = "APROVADO"
+        return  status
+    if conceito == "D" or conceito =="E":
+        status = "REPROVADO"
+        return  status
+
 
 resultado_nota_1 = digita_nota("Digite a primeira nota: ")
 resultado_nota_2 = digita_nota("Digite a segunda nota: ")
 resultado_media = calcula_media(resultado_nota_1,resultado_nota_2)
 resultado_conceito = verifica_conceito(resultado_media)
-print("Com a média ",resultado_media, " O conceito para o aluno é: ",resultado_conceito )
+resultado_status = verifica_status(resultado_conceito)
+print("Com a média ",resultado_media, " O conceito para o aluno é: ",resultado_conceito , "Status: ", resultado_status)
